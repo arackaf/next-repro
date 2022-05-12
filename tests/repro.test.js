a6722fa752e741fbefb7e531344189a9df047b16
Adam Rackis <adamrackis@hotmail.com>
2022-05-11 21:24:41 -0500

    add clarifying comment

--- tests/repro.test.js
+++ tests/repro.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as nextRouter from "next/router";
-//swap this line for what's above, and the test will pass
+//swap this line for what's above, and the test will pass, even though the line above is correct: useRouter is a named export in next/router
 //import nextRouter from "next/router";
 
 import { render } from "@testing-library/react";
