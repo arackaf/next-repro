87eb878cc0859fdfe39edf1e0d0adca6b1e398e6
Adam Rackis <adamrackis@hotmail.com>
2022-05-11 21:23:33 -0500

    Add test fix

--- tests/repro.test.js
+++ tests/repro.test.js
@@ -1,5 +1,8 @@
 import React from "react";
 import * as nextRouter from "next/router";
+//swap this line for what's above, and the test will pass
+//import nextRouter from "next/router";
+
 import { render } from "@testing-library/react";
 
 import { Foo } from "../Component";
